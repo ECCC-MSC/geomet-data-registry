fed8b8fe037030ee5872530a22282634b32e90d9
Louis-Philippe Rousseau Lambert <louis-philippe.rousseaulambert@canada.ca>
2019-06-28 15:04:58 +0000

    update name in script

--- geomet_data_registry/layer/radar_1km.py
+++ geomet_data_registry/layer/radar_1km.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #
-# Copyright (C) 2019 Tom Kralidis
+# Copyright (C) 2019 Louis-Philippe Rousseaul-Lambert 
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
