d53baf2d04ee4b7433f8cd68cba4fbd32e044ae1
Tom Kralidis <tomkralidis@gmail.com>
2021-08-12 16:21:49 +0000

    add nightly deploy script

---
+++ deploy/nightly/deploy-nightly.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+###############################################################################
+#
+# Copyright (C) 2021 Tom Kralidis
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+BASEDIR=/data/web/geomet-data-registry-nightly
+GDR_GITREPO=https://github.com/ECCC-MSC/geomet-data-registry.git
+DAYSTOKEEP=7
+
+# you should be okay from here
+
+DATETIME=`date +%Y%m%d`
+TIMESTAMP=`date +%Y%m%d.%H%M`
+NIGHTLYDIR=msc-geomet-data-registry-$TIMESTAMP
+
+echo "Deleting nightly builds > $DAYSTOKEEP days old"
+
+cd $BASEDIR
+
+for f in `find . -type d -name "geomet-data-registry-20*"`
+do
+    DATETIME2=`echo $f | awk -F- '{print $3}' | awk -F. '{print $1}'`
+    let DIFF=(`date +%s -d $DATETIME`-`date +%s -d $DATETIME2`)/86400
+    if [ $DIFF -gt $DAYSTOKEEP ]; then
+        rm -fr $f
+    fi
+done
+
+rm -fr latest
+echo "Generating nightly build for $TIMESTAMP"
+mkdir $NIGHTLYDIR
+cd $NIGHTLYDIR
+git clone $GDR_GITREPO -b docker-compose2
+cd geomet-data-registry
+docker-compose -f docker/docker-compose-nightly.yml down
+docker-compose -f docker/docker-compose-nightly.yml build
+docker-compose -f docker/docker-compose-nightly.yml up -d
+
+cd ../..
+
+ln -s $NIGHTLYDIR latest
